fe04cd5a450fefff4c0a605a5876825779633335
flover <flo.fd@yandex.ru>
2021-03-08 20:19:45 +0300

    done

--- tasks/dict_task/planet.py
+++ tasks/dict_task/planet.py
@@ -22,7 +22,17 @@ get_planet_name(3) -> 'Земля'
 
 
 def get_planet_name(planet_num: int) -> str:
-    result = None
+    planets = {
+        1: 'Меркурий',
+        2: 'Венера',
+        3: 'Земля',
+        4: 'Марс',
+        5: 'Юпитер',
+        6: 'Сатурн',
+        7: 'Уран',
+        8: 'Нептун',
+    }
+    result = planets[planet_num]
     return result
 
 

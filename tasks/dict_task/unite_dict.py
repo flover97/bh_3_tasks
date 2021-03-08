0c5a4aa9fd1eb396db33a314cee62ee522c9e0ff
flover <flo.fd@yandex.ru>
2021-03-08 20:24:25 +0300

    done

--- tasks/dict_task/unite_dict.py
+++ tasks/dict_task/unite_dict.py
@@ -8,7 +8,7 @@ from copy import deepcopy
 
 def unite_dict(dict_1: dict, dict_2: dict) -> dict:
     dict_1_copy = deepcopy(dict_1)
-    # TODO dict_1_copy обновить элементами dict_2
+    dict_1_copy.update(dict_2)
     return dict_1_copy
 
 
